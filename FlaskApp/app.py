965bbc37a8bcbf918d84ad6818753e2f8c2b9b67
Morgan <morgan.roberts@emory.edu>
2018-10-27 17:45:51 -0400

    demo1

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')	
 
+@app.route("/main.html")
+
+def home():
+	return render_template('main.html')
+
+@app.route("/social.html")
+
+def social():
+	return render_template('social.html')
 if __name__ == "__main__":
     app.run()
